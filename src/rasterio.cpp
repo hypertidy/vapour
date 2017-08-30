11743df18836e997e4629929e8077a314eadb68f
Michael Sumner <mdsumner@gmail.com>
2017-08-30 22:58:33 +1000

    doc

--- src/rasterio.cpp
+++ src/rasterio.cpp
@@ -90,13 +90,26 @@ List raster_info (const char* pszFilename)
 }
 
 
-//' Raster IO
+//' Raster IO (read)
 //'
+//' Read a window of data from a GDAL raster source. The first argument is the source
+//' name and the second is a 6-element `window` of offset, source dimension, and output dimension.
+//'
+//' This is analgous to the `rgdal` function `readGDAL` with its arguments `offset`,  `region.dim`
+//' and `output.dim`.
 //' @param filename data source
 //' @param window src_offset, src_dim, out_dim
 //' @export
 //' @examples
 //' f <- system.file("extdata", "sst.tif", package = "vapour")
+//' ## a 5*5 window from a 10*10 region
+//' raster_io(f, window = c(0, 0, 10, 10, 5, 5))
+//' ## find the information first
+//' ri <- raster_info(f)
+//' str(matrix(raster_io(f, c(0, 0, ri$dimXY, ri$dimXY))), ri$dimXY[1]))
+//' ## the method can be used to up-sample as well
+//' str(matrix(raster_io(f, window = c(0, 0, 10, 10, 15, 25)), 15))
+//' ## a future version will provide access to different methods
 // [[Rcpp::export]]
 NumericVector raster_io(CharacterVector filename, IntegerVector window)
 {
@@ -139,92 +152,3 @@ NumericVector raster_io(CharacterVector filename, IntegerVector window)
 }
 
 
-//' Raster IO
-//'
-//' @noRd
-//' @examples
-//' f <- system.file("extdata", "volcano.tif", package = "vapour")
-//' fact <- 2
-//' m <- matrix(rasterio(f, fact), ncol(volcano)/fact)
-//' image(m)
-//' contour(t(volcano), add = TRUE)
-NumericVector rasterio (const char* pszFilename, NumericVector subfact = 5)
-{
-
-  // need a check for subfact >= 0 :)
-  GDALDataset  *poDataset;
-  GDALAllRegister();
-  poDataset = (GDALDataset *) GDALOpen( pszFilename, GA_ReadOnly );
-  if( poDataset == NULL )
-  {
-    Rcpp::stop("cannot open dataset");
-  }
-
-  double        adfGeoTransform[6];
-  // printf( "Driver: %s/%s\n",
-  //         poDataset->GetDriver()->GetDescription(),
-  //         poDataset->GetDriver()->GetMetadataItem( GDAL_DMD_LONGNAME ) );
-  // printf( "Size is %dx%dx%d\n",
-  //         poDataset->GetRasterXSize(), poDataset->GetRasterYSize(),
-  //         poDataset->GetRasterCount() );
-  // if( poDataset->GetProjectionRef()  != NULL )
-  //   printf( "Projection is `%s'\n", poDataset->GetProjectionRef() );
-  if( poDataset->GetGeoTransform( adfGeoTransform ) == CE_None )
-  {
-    printf( "Origin = (%.6f,%.6f)\n",
-            adfGeoTransform[0], adfGeoTransform[3] );
-    printf( "Pixel Size = (%.6f,%.6f)\n",
-            adfGeoTransform[1], adfGeoTransform[5] );
-
-  }
-
-
-  GDALRasterBand  *poBand;
-  int             nBlockXSize, nBlockYSize;
-  int             bGotMin, bGotMax;
-  double          adfMinMax[2];
-  poBand = poDataset->GetRasterBand( 1 );
-  poBand->GetBlockSize( &nBlockXSize, &nBlockYSize );
-  // printf( "Block=%dx%d Type=%s, ColorInterp=%s\n",
-  //         nBlockXSize, nBlockYSize,
-  //         GDALGetDataTypeName(poBand->GetRasterDataType()),
-  //         GDALGetColorInterpretationName(
-  //           poBand->GetColorInterpretation()) );
-  adfMinMax[0] = poBand->GetMinimum( &bGotMin );
-  adfMinMax[1] = poBand->GetMaximum( &bGotMax );
-  if( ! (bGotMin && bGotMax) )
-    GDALComputeRasterMinMax((GDALRasterBandH)poBand, TRUE, adfMinMax);
-//  printf( "Min=%.3fd, Max=%.3f\n", adfMinMax[0], adfMinMax[1] );
-//  if( poBand->GetOverviewCount() > 0 )
-//    printf( "Band has %d overviews.\n", poBand->GetOverviewCount() );
-//  if( poBand->GetColorTable() != NULL )
-//    printf( "Band has a color table with %d entries.\n",
-//            poBand->GetColorTable()->GetColorEntryCount() );
-
-   float *pafScanline;
-   int nXSize = poBand->GetXSize();
-   int nYSize = poBand->GetYSize();
-   int sub = subfact[0];
-
-   float f_outXSize = nXSize/sub;
-   float f_outYSize = nYSize/sub;
-
-   int outXSize = f_outXSize;
-   int outYSize = f_outYSize;
-
-   if (outXSize < 1) outXSize = 1;
-   if (outYSize < 1) outYSize = 1;
-
-
-
-   pafScanline = (float *) CPLMalloc(sizeof(float)*outXSize*outYSize);
-   poBand->RasterIO( GF_Read, 0, 0, nXSize, nYSize,
-                     pafScanline, outXSize, outYSize, GDT_Float32,
-                     0, 0 );
-
-   // close up
-   GDALClose( (GDALDatasetH) poDataset );
-   NumericVector out(outXSize*outYSize);
-   for (int i = 0; i < (outXSize*outYSize); i++) out[i] = pafScanline[i];
-  return out;
-}
