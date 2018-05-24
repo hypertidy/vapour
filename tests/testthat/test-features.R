34c55e6b20754d39bf956fb6be447e37604d294f
Michael Sumner <mdsumner@gmail.com>
2018-05-25 01:06:15 +1000

    tiny bit more test

--- tests/testthat/test-features.R
+++ tests/testthat/test-features.R
@@ -2,15 +2,17 @@ context("test-features.R")
 
 
 f <- system.file("extdata", "sst_c.gpkg", package = "vapour")
-gbin <- vapour_read_geometry_cpp(f, what = "geometry")
-gjsn <- vapour_read_geometry_cpp(f, what = "text", textformat = "json")
-ggml <- vapour_read_geometry_cpp(f, what = "text", textformat = "gml")
-gkml <- vapour_read_geometry_cpp(f, what = "text", textformat = "kml")
-gwkt <- vapour_read_geometry_cpp(f, what = "text", textformat = "wkt")
-gext <- vapour_read_geometry_cpp(f, what = "extent")
 
 
 test_that("geometry read works", {
+
+  gbin <- vapour_read_geometry_cpp(f, what = "geometry")
+  gjsn <- vapour_read_geometry_cpp(f, what = "text", textformat = "json")
+  ggml <- vapour_read_geometry_cpp(f, what = "text", textformat = "gml")
+  gkml <- vapour_read_geometry_cpp(f, what = "text", textformat = "kml")
+  gwkt <- vapour_read_geometry_cpp(f, what = "text", textformat = "wkt")
+  gext <- vapour_read_geometry_cpp(f, what = "extent")
+
   gbin %>% expect_length(7L)
   gjsn %>% expect_length(7L)
   ggml %>% expect_length(7L)
@@ -24,5 +26,11 @@ test_that("geometry read works", {
   gkml[[1]] %>% expect_type("character")  %>% grepl("<MultiGeometry><LineString>", .) %>% expect_true()
   gwkt[[1]] %>% expect_type("character")   %>% grepl("MULTILINESTRING \\(\\(-16254", .) %>% expect_true()
   gext[[4]] %>% expect_type("double")  %>% trunc() %>% expect_identical(c(-9293382, 7088338, -6881739, 9067994))
+
+  expect_identical(gjsn, vapour_read_geometry_text(f))
+  expect_identical(ggml, vapour_read_geometry_text(f, textformat = "gml"))
+  expect_identical(gkml, vapour_read_geometry_text(f, textformat = "kml"))
+  expect_identical(gwkt, vapour_read_geometry_text(f, textformat = "wkt"))
+  expect_identical(gext, vapour_read_extent(f))
 
 })
