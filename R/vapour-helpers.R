fa895010bae9f9d563edfe4e23ab4e7c3fb203a4
Michael Sumner <mdsumner@gmail.com>
2018-08-21 13:58:38 +0000

    rename to vapour_geom_summary

--- R/vapour-helpers.R
+++ R/vapour-helpers.R
@@ -1,15 +1,8 @@
 ## helpers
 
-geom_summary <- function(dsource, layer = 0L, sql = "") {
+vapour_geom_summary <- function(dsource, layer = 0L, sql = "") {
   if (!is.numeric(layer)) layer <- index_layer(dsource, layer)
   extents <- vapour_read_extent(dsource, layer, sql)
-  # if (nchar(sql) > 1) {
-  #   warning("input SQL used to summarize attributes, leave as \"\" to apply default efficient LIMIT")
-  # } else {
-  #   layername <- vapour_layer_names(dsource, sql = sql)[layer + 1L]
-  #   sql <- sprintf("SELECT * FROM %s LIMIT 1", layername)
-  # }
-  # attrs  <- vapour_read_attributes(dsource, layer, sql)
   na_geoms <- unlist(lapply(extents, anyNA))
   list(geometry = !na_geoms,
        xmin = unlist(lapply(extents, "[", 1L)),
