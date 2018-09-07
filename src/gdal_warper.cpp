5ee4ede44a378157785a714692984adb47142483
Michael Sumner <mdsumner@gmail.com>
2018-09-07 02:53:46 +0000

    internal raster_warp function

--- src/gdal_warper.cpp
+++ src/gdal_warper.cpp
@@ -10,13 +10,83 @@ using namespace Rcpp;
 #include <string>
 
 // [[Rcpp::export]]
-CharacterVector raster_warp (CharacterVector src_filename, CharacterVector dst_filename)
+CharacterVector raster_warp (CharacterVector src_filename, CharacterVector dst_filename,
+                             CharacterVector dst_SRSproj, NumericVector dst_dimXY,
+                             NumericVector dst_resolution)
 {
   GDALDatasetH  hSrcDS, hDstDS;
   GDALDriverH hDriver;
   GDALDataType eDT;
   // Open input and output files.
   GDALAllRegister();
+
+
+  // Open the source file.
+  hSrcDS = GDALOpen(src_filename[0], GA_ReadOnly );
+  CPLAssert( hSrcDS != NULL );
+  // Create output with same datatype as first input band.
+  eDT = GDALGetRasterDataType(GDALGetRasterBand(hSrcDS,1));
+  // Get output driver (GeoTIFF format)
+  hDriver = GDALGetDriverByName( "GTiff" );
+  CPLAssert( hDriver != NULL );
+  // Get Source coordinate system.
+  const char *pszSrcWKT = NULL;
+  char *pszDstWKT = NULL; //different to GDAL warp API (no const)
+  pszSrcWKT = GDALGetProjectionRef( hSrcDS );
+  CPLAssert( pszSrcWKT != NULL && strlen(pszSrcWKT) > 0 );
+
+  OGRSpatialReference oSRS;
+  //oSRS.SetProjCS(dst_SRSproj[0]);
+  oSRS.importFromProj4(dst_SRSproj[0]);
+  //oSRS.SetUTM( 11, TRUE );
+  //oSRS.SetWellKnownGeogCS( "WGS84" );
+  oSRS.exportToWkt( &pszDstWKT );
+  CPLAssert( pszDstWKT != NULL && strlen(pszDstWKT) > 0 );
+  // More checks here, e.g. https://www.gdal.org/classOGRSpatialReference.html#a16c78a4cb401cccaa82a9e65f1b5d5c4
+//  oSRS.Validate();
+
+
+  // Create a transformer that maps from source pixel/line coordinates
+  // to destination georeferenced coordinates (not destination
+  // pixel line).  We do that by omitting the destination dataset
+  // handle (setting it to NULL).
+  void *hTransformArg;
+  hTransformArg =
+    GDALCreateGenImgProjTransformer( hSrcDS, pszSrcWKT, NULL, pszDstWKT,
+                                     FALSE, 0, 1 );
+  CPLAssert( hTransformArg != NULL );
+  // Get approximate output georeferenced bounds and resolution for file.
+  double adfDstGeoTransform[6];
+  int nPixels=0, nLines=0;
+  CPLErr eErr;
+  eErr = GDALSuggestedWarpOutput( hSrcDS,
+                                  GDALGenImgProjTransform, hTransformArg,
+                                  adfDstGeoTransform, &nPixels, &nLines );
+  CPLAssert( eErr == CE_None );
+  GDALDestroyGenImgProjTransformer( hTransformArg );
+
+
+  // Create the output file.
+  hDstDS = GDALCreate( hDriver, dst_filename[0], nPixels, nLines,
+                       GDALGetRasterCount(hSrcDS), eDT, NULL );
+  CPLAssert( hDstDS != NULL );
+
+
+  //Write out the projection definition.
+  GDALSetProjection( hDstDS, pszDstWKT );
+  GDALSetGeoTransform( hDstDS, adfDstGeoTransform );
+  // // Copy the color table, if required.
+  // GDALColorTableH hCT;
+  // hCT = GDALGetRasterColorTable( GDALGetRasterBand(hSrcDS,1) );
+  // if( hCT != NULL )
+  //   GDALSetRasterColorTable( GDALGetRasterBand(hDstDS,1), hCT );
+  //
+  //
+  //
+
+
+
+
   // Setup warp options.
   GDALWarpOptions *psWarpOptions = GDALCreateWarpOptions();
   psWarpOptions->hSrcDS = hSrcDS;
@@ -37,58 +107,6 @@ CharacterVector raster_warp (CharacterVector src_filename, CharacterVector dst_f
                                      GDALGetProjectionRef(hDstDS),
                                      FALSE, 0.0, 1 );
   psWarpOptions->pfnTransformer = GDALGenImgProjTransform;
-
-  // Open the source file.
-  hSrcDS = GDALOpen(src_filename[0], GA_ReadOnly );
-  CPLAssert( hSrcDS != NULL );
-  // Create output with same datatype as first input band.
-  eDT = GDALGetRasterDataType(GDALGetRasterBand(hSrcDS,1));
-  // Get output driver (GeoTIFF format)
-  hDriver = GDALGetDriverByName( "GTiff" );
-  CPLAssert( hDriver != NULL );
-  // Get Source coordinate system.
-  const char *pszSrcWKT = NULL;
-  char *pszDstWKT = NULL; //different to GDAL warp API (no const)
-  pszSrcWKT = GDALGetProjectionRef( hSrcDS );
-  CPLAssert( pszSrcWKT != NULL && strlen(pszSrcWKT) > 0 );
-  // Setup output coordinate system that is UTM 11 WGS84.
-  OGRSpatialReference oSRS;
-  oSRS.SetUTM( 11, TRUE );
-  oSRS.SetWellKnownGeogCS( "WGS84" );
-  oSRS.exportToWkt( &pszDstWKT );
-  // Create a transformer that maps from source pixel/line coordinates
-  // to destination georeferenced coordinates (not destination
-  // pixel line).  We do that by omitting the destination dataset
-  // handle (setting it to NULL).
-  void *hTransformArg;
-  hTransformArg =
-    GDALCreateGenImgProjTransformer( hSrcDS, pszSrcWKT, NULL, pszDstWKT,
-                                     FALSE, 0, 1 );
-  CPLAssert( hTransformArg != NULL );
-  // Get approximate output georeferenced bounds and resolution for file.
-  double adfDstGeoTransform[6];
-  int nPixels=0, nLines=0;
-  CPLErr eErr;
-  eErr = GDALSuggestedWarpOutput( hSrcDS,
-                                  GDALGenImgProjTransform, hTransformArg,
-                                  adfDstGeoTransform, &nPixels, &nLines );
-  CPLAssert( eErr == CE_None );
-  GDALDestroyGenImgProjTransformer( hTransformArg );
-  // Create the output file.
-  hDstDS = GDALCreate( hDriver, dst_filename[0], nPixels, nLines,
-                       GDALGetRasterCount(hSrcDS), eDT, NULL );
-  CPLAssert( hDstDS != NULL );
-  // Write out the projection definition.
-  GDALSetProjection( hDstDS, pszDstWKT );
-  GDALSetGeoTransform( hDstDS, adfDstGeoTransform );
-  // // Copy the color table, if required.
-  // GDALColorTableH hCT;
-  // hCT = GDALGetRasterColorTable( GDALGetRasterBand(hSrcDS,1) );
-  // if( hCT != NULL )
-  //   GDALSetRasterColorTable( GDALGetRasterBand(hDstDS,1), hCT );
-
-
-
   // Initialize and execute the warp operation.
   GDALWarpOperation oOperation;
   oOperation.Initialize( psWarpOptions );
@@ -97,6 +115,9 @@ CharacterVector raster_warp (CharacterVector src_filename, CharacterVector dst_f
                                 GDALGetRasterYSize( hDstDS ) );
   GDALDestroyGenImgProjTransformer( psWarpOptions->pTransformerArg );
   GDALDestroyWarpOptions( psWarpOptions );
+
+
+
   GDALClose( hDstDS );
   GDALClose( hSrcDS );
   return "";
