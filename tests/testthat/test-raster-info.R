218327c4bd3e6809d68bb2f3ab71896c6c044c63
Michael Sumner <mdsumner@gmail.com>
2019-02-01 03:18:32 +0000

    sds test

--- tests/testthat/test-raster-info.R
+++ tests/testthat/test-raster-info.R
@@ -5,8 +5,13 @@ test_that("raster info works", {
   expect_error(vapour_sds_names(), 'argument "x" is missing, with no default')
   expect_error(vapour_sds_names(""), 'cannot open dataset')
 
+
+  expect_message(sds_boilerplate_checks(f), "subdataset \\(variable\\) used is '//f16'")
+  expect_error(sds_boilerplate_checks(f, 0), "sds must be 1 at minimum")
+  expect_match(sds_boilerplate_checks(f, 1),
+               "^HDF5:\".*extdata/gdal/complex\\.h5\"://f16$")
+
   expect_silent(s1 <- vapour_sds_names(f)) %>% expect_named(c("datasource", "subdataset"))
   expect_length(unlist(s1), 6L)
-
 
 })
