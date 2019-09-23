b9c7c80995039499375f79b08dc07bb8832f49cc
Michael Sumner <mdsumner@gmail.com>
2019-09-23 01:40:40 +0000

    kill flaky os-specific tests

--- tests/testthat/test-raster-info.R
+++ tests/testthat/test-raster-info.R
@@ -35,12 +35,16 @@ test_that("raster info works", {
   expect_error(vapour_sds_names(), 'argument "x" is missing, with no default')
   expect_error(vapour_sds_names(""), 'cannot open dataset')
 
-  expect_message(sds_boilerplate_checks(f), "subdataset \\(variable\\) used is '//f16'")
-  expect_error(sds_boilerplate_checks(f, 0), "sds must be 1 at minimum")
-  expect_match(sds_boilerplate_checks(f, 1),
-               "^HDF5:\".*extdata/gdal/complex\\.h5\"://f16$")
 
+  expect_error(sds_boilerplate_checks(f, 0), "sds must be 1 at minimum")
+  ## these are different on Windows and Linux ...
+  # expect_message(sds_boilerplate_checks(f), "subdataset \\(variable\\) used is '//f16'")
+  #
+  # expect_match(sds_boilerplate_checks(f, 1),
+  #              "^HDF5:\".*extdata/gdal/complex\\.h5\"://f16$")
+  #
   expect_silent(s1 <- vapour_sds_names(f)) %>% expect_named(c("datasource", "subdataset"))
   expect_length(unlist(s1), 6L)
 
 })
+
