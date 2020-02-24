256a6c1df523db11627605ef1a7c4dcfc1577f93
Michael Sumner <mdsumner@gmail.com>
2020-02-24 01:18:05 +0000

    add overview example

--- data-raw/volcano.R
+++ data-raw/volcano.R
@@ -1,3 +1,6 @@
 library(raster)
 v <- setExtent(raster(volcano), extent(0, ncol(volcano), 0, nrow(volcano)))
 writeRaster(v, "inst/extdata/volcano.tif")
+
+file.copy("inst/extdata/volcano.tif", "inst/extdata/volcano_overview.tif")
+system("gdaladdo inst/extdata/volcano_overview.tif -minsize 4 2 4 8 16")
