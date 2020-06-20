d07323c162d2eb2eb8ad8eb086cc6044e2b24b7a
Michael Sumner <mdsumner@gmail.com>
2020-06-20 23:27:20 +0000

    config

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -16,7 +16,4 @@ test_that("utilities work", {
   expect_error(vapour_driver("1"), "Open failed")
   expect_match(vapour_driver(mvfile), "MapInfo File")
 
-  gdaldata <- Sys.getenv("GDAL_DATA")
-  print("GDAL_DATA: ")
-  print(gdaldata)
 })
