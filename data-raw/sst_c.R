a04ca5620a8e56e2ff95a130711684543dca16a1
Michael Sumner <mdsumner@gmail.com>
2021-07-21 05:47:18 +1000

    remove old +init syntax (seen on clang-UBSAN on cran)

--- data-raw/sst_c.R
+++ data-raw/sst_c.R
@@ -2,7 +2,8 @@ library(raadtools)
 
 sst <- readghrsst(latest = TRUE, xylim = extent(-180, 180, -90, 0))
 cl <- raster::rasterToContour(sst)
-cl <- sp::spTransform(cl, "+init=epsg:3031")
+## epsg:3031
+cl <- sp::spTransform(cl, "+proj=stere +lat_0=-90 +lat_ts=-71 +datum=WGS84")
 cl <- sf::st_as_sf(cl)
 cl$sst <- as.numeric(levels(cl$level)[cl$level]) - 273.15
 sst_c <- cl
--- tests/testthat/test-test-on-rhub.R
+++ tests/testthat/test-test-on-rhub.R
@@ -1,3 +1,3 @@
 test_that("multiplication works", {
-  vapour::vapour_srs_wkt("+init=epsg:4326")
+  vapour::vapour_srs_wkt("+proj=longlat +datum=WGS84")
 })
