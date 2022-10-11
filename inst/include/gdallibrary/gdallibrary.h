6535684c95d214c0891e84488b8143582e0811b7
Michael Sumner <mdsumner@gmail.com>
2022-10-11 21:35:37 +1100

    fix some implicit conversions?

--- inst/include/gdallibrary/gdallibrary.h
+++ inst/include/gdallibrary/gdallibrary.h
@@ -167,13 +167,13 @@ inline List gdal_list_drivers()
   Rcpp::LogicalVector isvirt(n);
   for (int idriver = 0; idriver < n; idriver++) {
     GDALDriver *dr = GetGDALDriverManager()->GetDriver(idriver);
-    sname(idriver) = GDALGetDriverShortName(dr);
-    lname(idriver) = GDALGetDriverLongName(dr);
-    isvector(idriver) = (dr->GetMetadataItem(GDAL_DCAP_VECTOR) != NULL);
-    israster(idriver) = (dr->GetMetadataItem(GDAL_DCAP_RASTER) != NULL);
-    iscopy(idriver) = (dr->GetMetadataItem(GDAL_DCAP_CREATECOPY) != NULL);
-    iscreate(idriver) = (dr->GetMetadataItem(GDAL_DCAP_CREATE) != NULL);
-    isvirt(idriver) = (dr->GetMetadataItem(GDAL_DCAP_VIRTUALIO) != NULL);
+    sname[idriver] = GDALGetDriverShortName(dr);
+    lname[idriver] = GDALGetDriverLongName(dr);
+    isvector[idriver] = (dr->GetMetadataItem(GDAL_DCAP_VECTOR) != NULL);
+    israster[idriver] = (dr->GetMetadataItem(GDAL_DCAP_RASTER) != NULL);
+    iscopy[idriver] = (dr->GetMetadataItem(GDAL_DCAP_CREATECOPY) != NULL);
+    iscreate[idriver] = (dr->GetMetadataItem(GDAL_DCAP_CREATE) != NULL);
+    isvirt[idriver] = (dr->GetMetadataItem(GDAL_DCAP_VIRTUALIO) != NULL);
   }
   Rcpp::List out = Rcpp::List::create(Rcpp::Named("driver") = sname,
                                       Rcpp::Named("name") = lname,
