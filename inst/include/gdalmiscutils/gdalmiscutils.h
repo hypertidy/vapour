9a289434e50bc6d71209729bc34c2b2647baee67
Michael Sumner <mdsumner@gmail.com>
2022-11-12 00:04:40 +1100

    fix clang warnings on lossy

--- inst/include/gdalmiscutils/gdalmiscutils.h
+++ inst/include/gdalmiscutils/gdalmiscutils.h
@@ -24,7 +24,7 @@ inline NumericVector limit_skip_to_start_end(IntegerVector skip_n, IntegerVector
 
 inline NumericVector limit_skip_n_to_start_end_len(IntegerVector skip_n, IntegerVector limit_n, NumericVector n) {
   R_xlen_t start = 0;
-  R_xlen_t end = n[0] - 1;
+  R_xlen_t end = (R_xlen_t)n[0] - 1;
   if (skip_n[0] > 0) {  // silently ignore negative values
     start = (R_xlen_t)skip_n[0];
   }
@@ -34,16 +34,16 @@ inline NumericVector limit_skip_n_to_start_end_len(IntegerVector skip_n, Integer
   if (is_infinite(skip_n)[0]) {
     Rcpp::warning("skip_n not a valid value, assuming 'skip_n = 0'"); 
   }
-  if (n[0] > 0 && start >= n[0] ) {
+  if (n[0] > 0 && start >= (R_xlen_t)n[0] ) {
     Rcpp::stop("skip_n skips all available features");
   }
-  if (end > (n[0] - 1)) {
+  if (end > ((R_xlen_t)n[0] - 1)) {
     if (start > 0) {
       Rcpp::warning("limit_n is greater than the number of available features (given 'skip_n')");
     } else {
       Rcpp::warning("limit_n is greater than the number of available features");
     }
-    end = n[0] - 1;
+    end = (R_xlen_t)n[0] - 1;
   }
   R_xlen_t len = end - start + 1;
   NumericVector out(3);
