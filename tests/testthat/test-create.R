4eff74d8b1a3427e0d3ffe6e2f5f50de9148d5c3
Michael Sumner <mdsumner@gmail.com>
2024-06-03 08:15:40 +0000

    change create output

--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -1,6 +1,6 @@
 ex <- c(10, 20, -10, 20)
 test_that("create works", {
-  expect_output(vapour_create(tf <- tempfile(fileext = ".tif"), dimension = c(10, 5), extent = ex)) |> expect_equal(tf)
+  expect_silent(vapour_create(tf <- tempfile(fileext = ".tif"), dimension = c(10, 5), extent = ex)) |> expect_equal(tf)
   expect_silent(info <- vapour_raster_info(tf))  
   expect_equal(info$extent, ex)
   expect_equal(info$dimension, c(10, 5))
