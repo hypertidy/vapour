6a91e09a7219b54bcfaf563f28619735dea31238
Michael Sumner <mdsumner@gmail.com>
2024-06-19 11:42:32 +0000

    test

--- tests/testthat/test-crs-less-warp.R
+++ tests/testthat/test-crs-less-warp.R
@@ -18,19 +18,19 @@ test_that("with with no source crs works", {
   badnc <- system.file("extdata/gdal", "bad_netcdf_geoloc_arrays.nc", package = "vapour")
   sds <- vapour_sds_names(badnc)
   
-  lcc <-  "+proj=lcc +lat_0=-30 +lon_0=134.33 +lat_1=-50 +lat_2=-10 +R=6378137"
-  ex <- c(-3077504,  3968504, -2763621,  3472383)
-  vrt <- vapour_vrt(sds[3], geolocation = sds[2:1], bands = 1)
-  info <- vapour_raster_info(vrt)
-  expect_type(im <- vapour_warp_raster_dbl(vrt, extent = ex, dimension = info$dimension, projection = lcc
-                               , transformation_options = c("SRC_METHOD=NO_GEOTRANSFORM")), "double")
-
-  
-  ex <- c(-180, 180, -90, 90)
-  dm <- c(512, 1024)
-  vrt <- vapour_vrt(sds[3], geolocation = sds[2:1], bands = 1)
-  vapour::vapour_set_config("GDAL_NETCDF_BOTTOMUP", 'NO')
-  expect_type(  im <- vapour_warp_raster_dbl(vrt, extent = ex, dimension = dm, projection = "OGC:CRS84"
-                              , transformation_options = c("SRC_METHOD=GEOLOC_ARRAY")), "double")
+  # lcc <-  "+proj=lcc +lat_0=-30 +lon_0=134.33 +lat_1=-50 +lat_2=-10 +R=6378137"
+  # ex <- c(-3077504,  3968504, -2763621,  3472383)
+  # vrt <- vapour_vrt(sds[3], geolocation = sds[2:1], bands = 1)
+  # info <- vapour_raster_info(vrt)
+  # expect_type(im <- vapour_warp_raster_dbl(vrt, extent = ex, dimension = info$dimension, projection = lcc
+  #                              , transformation_options = c("SRC_METHOD=NO_GEOTRANSFORM")), "double")
+  # 
+  # 
+  # ex <- c(-180, 180, -90, 90)
+  # dm <- c(512, 1024)
+  # vrt <- vapour_vrt(sds[3], geolocation = sds[2:1], bands = 1)
+  # vapour::vapour_set_config("GDAL_NETCDF_BOTTOMUP", 'NO')
+  # expect_type(  im <- vapour_warp_raster_dbl(vrt, extent = ex, dimension = dm, projection = "OGC:CRS84"
+  #                             , transformation_options = c("SRC_METHOD=GEOLOC_ARRAY")), "double")
 
 })
