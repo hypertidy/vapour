2364e6d27012b7766abf39b2fdafa8daedb29567
Michael Sumner <mdsumner@gmail.com>
2026-03-09 02:09:31 +0000

    cleanup

--- inst/conversion/cpp11-examples/00_utils.cpp
+++
@@ -1,9 +0,0 @@
-#include <cpp11.hpp>
-#include "gdalapplib/gdalapplib.h"
-
-using namespace cpp11;
-
-[[cpp11::register]]
-strings raster_gdalinfo_app_cpp(strings dsn, strings options) {
-  return gdalapplib::gdalinfo_applib_cpp(dsn, options);
-}
